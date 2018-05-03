154aa4f12f7dc351bd915d1beb202b8bd02e6021
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-03 04:54:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,5 @@ class BoardMember {
   approve() {return "You can do that!"}
   doCharity() {return "I like to help people."}
   releasePressStatement() {return "You will see great things from Scuber."}
-  veto() {return "No, I must disagree"}
-  veto() {return "No, I must disagree"}
+  sayHi() {return Hi, my name is <name>. I am from <homestate>, and I was trained in <training>."}
 }
